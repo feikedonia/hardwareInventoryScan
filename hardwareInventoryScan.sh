ffa5813e542439191cec3d6f741a8f3363ba1edb
Feike Donia <feikedonia@proton.me>
2024-10-07 13:30:26 +0200

    hardware

--- hardwareInventoryScan.sh
+++ hardwareInventoryScan.sh
@@ -2,9 +2,15 @@
 # Copyright 2024 Feike Donia
 # Hardware Inventory Scan
 
+# Check to run as root(admin), otherwise fail
+if [ "$(id -u)" != 0 ]; then
+	echo "Dit script heeft root(admin) permissies nodig om te draaien."
+	echo "Gelieve dit script te heropenen met sudo of een root shell."
+	exit 1 
+fi
 #Check the dependancies
 
-notFount=""
+notFount="" 
 
 if ! which vim &> /dev/null; then   # if curl does not exist, add curl to the notFount variable
   notFount+="vim "
