d9d90609d49714b0581154a425b232e2237c97fa
Feike Donia <feikedonia@proton.me>
2024-11-13 18:03:29 +0100

    fix: matching if...fi statements

--- util-linux.sh
+++ util-linux.sh
@@ -11,17 +11,17 @@ if ! command -v lscpu &> /dev/null; then
 	#try to install
 	echo "De programma-bundel util-linux is niet geïnstalleerd."
 	echo "Automatische installatie wordt geprobeerd..."
-
 	if command -v apt &> /dev/null; then
-			apt install -y util-linux
-		elif command -v dnf &> /dev/null; then
-			dnf in -y util-linux
-		elif command -v pacman &> /dev/null; then
-			pacman -S --noconfirm util-linux
-		else
-			echo "De automatische installatie is gefaald."
-			exit 1
-		fi
+		apt install -y util-linux
+	elif command -v dnf &> /dev/null; then
+		dnf in -y util-linux
+	elif command -v pacman &> /dev/null; then
+		pacman -S --noconfirm util-linux
+	else
+		echo "De automatische installatie is gefaald."
+		exit 1
+	fi
+fi
 
 #Check the system language
 if [ "$LANG" != "en_US.UTF-8" ]; then
