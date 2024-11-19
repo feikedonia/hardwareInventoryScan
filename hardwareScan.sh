a45021738fa5db55cce2e0db6b57e534057eca3d
Feike Donia <feikedonia@proton.me>
2024-11-19 15:07:33 +0100

    update hardwareScan.sh

--- hardwareScan.sh
+++ hardwareScan.sh
@@ -3,31 +3,31 @@
 #GPL-2.0-or-later
 
 scriptName="hardwareScan.sh"
-echo "$scriptName wordt uitgevoerd..."
+echo ":::$scriptName wordt uitgevoerd..."
 
-echo "Check of script wordt uitgevoerd als root..."
+echo ":::Check of script wordt uitgevoerd als root..."
 if [ "$(id -u)" != 0 ]; then 
-	echo "Dit script heeft root(admin) rechten nodig om te draaien."
-	echo "Gelieve dit script te heropenen in root shell:"
-	echo "'su' of 'sudo ./hardwareScan'."
+	echo "!::Dit script heeft root(admin) rechten nodig om te draaien."
+	echo "!::Gelieve dit script te heropenen in root shell:"
+	echo "!::'su' of 'sudo ./hardwareScan'."
 	exit 1
 fi
 
-echo "Check of u in de juiste map staat..."
+echo ":::Check of u in de juiste map staat..."
 if [ "$(pwd)" != "$HOME/hardwareInventoryScan" ]; then
 	if ! [ -d "$HOME/hardwareInventoryScan" ]; then
-		echo "Kan de hardwareInventoryScan map niet vinden."
+		echo "!::Kan de hardwareInventoryScan map niet vinden."
 		exit 1
 	fi
 	cd "$HOME/hardwareInventoryScan"
 fi
 	
 #Make temp directory for the outputs to go to
-echo "Mappen aanmaken..."
+echo ":::Mappen aanmaken..."
 mkdir -p /tmp/hardwareScan/
 
 #Check if updated recently
-echo "Check of het systeem recent in geüpdatet..."
+echo ":::Check of het systeem recent in geüpdatet..."
 checkFile="/tmp/hardwareScan/timestamp.txt"
 
 if [ -f "$checkFile" ]; then
@@ -36,27 +36,11 @@ if [ -f "$checkFile" ]; then
 		#if older then 3 days; then update.sh
 		./update.sh
 	else
-		echo "Systeem is recent geüpdatet."
+		echo ":::Systeem is recent geüpdatet."
 	fi
 else
-	echo "Systeem is recent geüpdatet."
+	echo ":::Systeem is recent geüpdatet."
 fi
-
-#Update the system, before running the scans
-echo "Automatisch updaten..."
-if command -v apt &> /dev/null; then
-	echo "Debian / Ubuntu gededecteerd, APT wordt geüpdatet..."
-	apt update && apt upgrade -y
-elif command -v dnf &> /dev/null; then
-	echo "RHEL / CentOS / Fedora gededecteerd, DNF wordt geüpdatet..."
-	dnf check-update && dnf upgrade -y
-elif command -v pacman &> /dev/null; then
-	echo "Arch linux gededecteerd, Pacman wordt geüpdatet..."
-	pacman -Syyu --noconfirm
-else
-	echo "Automatisch updaten mislukt."
-	exit 1
-fi	
 
 #Start the next script, without preserved variables
 ./util-linux.sh
--- update.sh
+++ update.sh
@@ -4,29 +4,29 @@
 
 #name and function
 scriptName="update.sh"
-echo "$scriptName wordt uitgevoerd..."
+echo ":::$scriptName wordt uitgevoerd..."
 
 #Update the system, before running the scans
-echo "Automatisch updaten..."
+echo ":::Automatisch updaten..."
 if command -v apt &> /dev/null; then
-	echo "Debian / Ubuntu gededecteerd, APT wordt geüpdatet..."
+	echo ":::Debian / Ubuntu gededecteerd, APT wordt geüpdatet..."
 	apt update && apt upgrade -y
 	#Make timestamp or update timestamp
 	echo "$(date +%s)" > /tmp/hardwareScan/timestamp.txt
 	
 elif command -v dnf &> /dev/null; then
-	echo "RHEL / CentOS / Fedora gededecteerd, DNF wordt geüpdatet..."
+	echo ":::RHEL / CentOS / Fedora gededecteerd, DNF wordt geüpdatet..."
 	dnf check-update && dnf upgrade -y
 	#Make timestamp or update timestamp
 	echo "$(date +%s)" > /tmp/hardwareScan/timestamp.txt
 	
 elif command -v pacman &> /dev/null; then
-	echo "Arch linux gededecteerd, Pacman wordt geüpdatet..."
+	echo ":::Arch linux gededecteerd, Pacman wordt geüpdatet..."
 	pacman -Syyu --noconfirm
 	#Make timestamp or update timestamp
 	echo "$(date +%s)" > /tmp/hardwareScan/timestamp.txt
 	
 else
-	echo "Automatisch updaten mislukt."
+	echo "!::Automatisch updaten mislukt."
 	exit 1
 fi	
